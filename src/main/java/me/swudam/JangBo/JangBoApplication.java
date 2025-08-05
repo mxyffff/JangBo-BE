48824cfffdc62416e3a4ea3312f38a56a94ca56a
mxyffff <rua0254@naver.com>
2025-08-06 00:03:21 +0900

    Gradle 설정에서 데이터베이스 관련 설정 주석처리함(DB 설계시 주석처리 해제 필요), 메인함수 작성

---
+++ src/main/java/me/swudam/JangBo/JangBoApplication.java
@@ -0,0 +1,11 @@
+package me.swudam.JangBo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangBoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JangBoApplication.class, args);
+    }
+}
