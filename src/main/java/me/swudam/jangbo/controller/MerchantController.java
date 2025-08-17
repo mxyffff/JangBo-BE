e0dcd3b8c985b76eae5fe6786a338d191215278e
Rainshn <rainshn1@gmail.com>
2025-08-17 23:08:23 +0900

    임시 서버 테스트용 로그 찍기

--- src/main/java/me/swudam/jangbo/controller/MerchantController.java
+++ src/main/java/me/swudam/jangbo/controller/MerchantController.java
@@ -43,7 +43,10 @@ public class MerchantController {
     // 상인 회원가입 API
     // POST - /api/merchants/signup
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@Valid @RequestBody MerchantFormDto dto, HttpSession session) {
+    public ResponseEntity<?> signup(@Valid @RequestBody MerchantFormDto dto, HttpSession session, HttpServletRequest request) {
+        System.out.println("Request URI: " + request.getRequestURI());
+        System.out.println("HTTP Method: " + request.getMethod());
+
         if (!dto.getPassword().equals(dto.getPasswordConfirm())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
